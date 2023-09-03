37cac3f9e976c6a64fb894590fbd606605d74eab
lawzheng <602285510@qq.com>
2023-09-03 18:07:34 +0800

    支持第三方库的解析

--- index.js
+++ index.js
@@ -6,15 +6,47 @@ const app = new Koa();
 app.use(async ctx => {
   const { url, query } = ctx;
   console.log('url:', url)
+  // index.html
   if (url === '/') {
     ctx.type = 'text/html'
-    const content = fs.readFileSync('./index.html', 'utf-8')
+    let content = fs.readFileSync('./index.html', 'utf-8')
+    // 加环境变量
+    content = content.replace('<script', `
+    <script>
+      window.process = { env: { NODE_ENV: 'dev' } } 
+    </script>
+    <script`)
     ctx.body = content
-  } else if(url.endsWith('.js')) {
+  }
+  // *.js
+  else if (url.endsWith('.js')) {
     const p = path.resolve(__dirname, url.slice(1))
     const content = fs.readFileSync(p, 'utf-8')
     ctx.type = 'application/javascript'
-    ctx.body = content
+    ctx.body = rewriteImport(content)
+  }
+  // 第三方库
+  else if (url.startsWith('/@modules')) {
+    const prefix = path.resolve(
+      __dirname,
+      'node_modules',
+      url.replace('/@modules/', '')
+    )
+    const module = require(prefix + '/package.json').module;
+    const p = path.resolve(prefix, module)
+    const content = fs.readFileSync(p, 'utf-8')
+    ctx.type = 'application/javascript'
+    ctx.body = rewriteImport(content)
+  }
+
+  function rewriteImport(content) {
+    return content.replace(/ from ['|"]([^'"]+)['|"]/g, function (s0, s1) {
+      if (s1[0] !== '.' && s1[1] !== '/') {
+        return ` from '/@modules/${s1}'`
+      } else {
+        return s0
+      }
+    })
   }
 })
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import { a } from './a.js'
-
 console.log('vite', a)
+import { createApp, h } from 'vue';
+
+const App = {
+  render() {
+    return h('div', null, [h('div', null, String('hello vue'))])
+  }
+}
+
+createApp(App).mount('#app')
